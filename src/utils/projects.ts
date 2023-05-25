d66e472da4991bd930ec0403e4a1d8347b53deba
EddieWebDev <eddandersson@gmail.com>
2023-05-25 15:55:44 +0200

    fixed img url path

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,37 +1,37 @@
 export const projects = [
   {
     title: "Project one",
-    imgUrl: "/public/hero2.jpg",
+    imgUrl: "/hero2.jpg",
     description:
       "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable",
   },
   {
     title: "Project two",
-    imgUrl: "/public/hero2.jpg",
+    imgUrl: "/hero2.jpg",
     description:
       "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable",
   },
   {
     title: "Project three",
-    imgUrl: "/public/hero2.jpg",
+    imgUrl: "/hero2.jpg",
     description:
       "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable",
   },
   {
     title: "Project four",
-    imgUrl: "/public/hero2.jpg",
+    imgUrl: "/hero2.jpg",
     description:
       "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable",
   },
   {
     title: "Project five",
-    imgUrl: "/public/hero2.jpg",
+    imgUrl: "/hero2.jpg",
     description:
       "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable",
   },
   {
     title: "Project six",
-    imgUrl: "/public/hero2.jpg",
+    imgUrl: "/hero2.jpg",
     description:
       "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable",
   },
