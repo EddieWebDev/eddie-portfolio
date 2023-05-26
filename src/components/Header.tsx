94c2ea38c011a252ab024611541ee7b611fd24e5
EddieWebDev <eddandersson@gmail.com>
2023-05-26 15:20:41 +0200

    added section ids

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
 export const Header = () => {
   return (
     <header className="h-96 bg-hero-image bg-cover flex flex-col ">
-      <div className="flex flex-row-reverse gap-4 p-4">
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
+      <div className="flex flex-row-reverse gap-4 p-4 text-white">
+        <a href="#about"><p>About</p></a>
+        <a href="#skills"><p>Skills</p></a>
+        <a href="#portfolio"><p>Portfolio</p></a>
+        <a href="#contact"><p>Contact</p></a>
       </div>
       <div className=" flex flex-col items-center justify-center gap-12 h-full">
         <h1 className="text-center text-4xl text-white font-bold">
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,7 +3,7 @@ import { skills } from "Utils/skills";
 
 export const Skills = () => {
   return (
-    <section className="flex flex-col items-center p-8">
+    <section id="skills" className="flex flex-col items-center p-8">
       <p className="text-3xl font-bold py-4">My skills</p>
       <div className="flex gap-4 justify-center flex-wrap py-4 max-w-4xl">
         {skills.map((skill) => (
